403683e3162cf909567881a30a2cdc8483b708c7
Quarzi <kim.jan.andersen82@gmail.com>
2015-11-28 06:49:02 +0100

    Video block 4 done

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Mvc;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Couldt not send email, configuration error!");
+                }
+
+                if (_mailService.SendMail(email,
+                    email,
+                    $"Contact Page From {model.Name} ({model.Email})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent, Thanks!!";
+                }
+                
+            }
+
+            return View();
+        }
+    }
+}
